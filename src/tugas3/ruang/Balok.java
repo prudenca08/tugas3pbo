0eb7654bcd97cf662d096c7bfd6d23718efb2f99
Daffa Github <63253142+prudenca08@users.noreply.github.com>
2021-04-11 10:15:56 +0700

    Update Balok.java

--- src/tugas3/ruang/Balok.java
+++ src/tugas3/ruang/Balok.java
@@ -3,12 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package tugas3.ruang;
 
 /**
  *
- * @author Daffa
+ * Nama    : Prudenca Ahmad Daffa K.
+   NIM : 123190005
  */
+package tugas3.ruang;
 public class Balok implements MenghitungRuang {
 
     private int panjang,lebar,tinggi;
