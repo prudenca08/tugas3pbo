03b6474d267c826db5f3cde2b3c96ad8841c02b4
Daffa Github <63253142+prudenca08@users.noreply.github.com>
2021-04-11 10:06:31 +0700

    Update tugas3.java

--- src/main/tugas3.java
+++ src/main/tugas3.java
@@ -3,6 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
+/**
+ *
+ * Nama    : Prudenca Ahmad Daffa K.
+   NIM : 123190005
+ */
 package main;
 
 import java.util.Scanner;
